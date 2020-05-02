797e49f90850fc5262c94ee7df70809bbaba294b
jacob <jrochest@gmail.com>
2020-05-02 08:54:21 -0700

    cleanups

--- app/src/main/java/com/md/SpacedRepeaterActivity.kt
+++ app/src/main/java/com/md/SpacedRepeaterActivity.kt
@@ -19,14 +19,10 @@ import android.support.v4.media.MediaBrowserCompat
 import android.support.v4.media.MediaMetadataCompat
 import android.support.v4.media.session.MediaControllerCompat
 import android.support.v4.media.session.PlaybackStateCompat
-import android.util.Log
 import android.view.KeyEvent
 import android.view.Menu
 import com.md.modesetters.*
 import java.io.*
-import java.util.zip.ZipEntry
-import java.util.zip.ZipInputStream
-import java.util.zip.ZipOutputStream
 
 
 class SpacedRepeaterActivity : Activity() {
@@ -408,71 +404,5 @@ class SpacedRepeaterActivity : Activity() {
 
         }
     }
-}
-
-object ZipManager {
-    fun zip(_files: Array<String>, zipFileName: String) {
-        try {
-            var origin: BufferedInputStream
-            val dest = FileOutputStream(zipFileName)
-            val out = ZipOutputStream(BufferedOutputStream(
-                    dest))
-            val data = ByteArray(BUFFER)
-            for (i in _files.indices) {
-                Log.v("Compress", "Adding: " + _files[i])
-                val fi = FileInputStream(_files[i])
-                origin = BufferedInputStream(fi, BUFFER)
-                val entry = ZipEntry(_files[i].substring(_files[i].lastIndexOf("/") + 1))
-                out.putNextEntry(entry)
-                var count: Int = 0
-                while (origin.read(data, 0, BUFFER).also { count = it } != -1) {
-                    out.write(data, 0, count)
-                }
-                origin.close()
-            }
-            out.flush()
-            out.close()
-        } catch (e: Exception) {
-            e.printStackTrace()
-        }
-    }
-
-    fun unzip(_zipFile: String?, _targetLocation: String) {
-        //create target location folder if not exist
-        dirChecker(_targetLocation)
-        try {
-            val fin = FileInputStream(_zipFile)
-            val zin = ZipInputStream(fin)
-            var ze: ZipEntry? = null
-            while (zin.getNextEntry().also({ ze = it }) != null) {
-
-                val ze = ze ?: continue
-                //create dir if required while unzipping
-                if (ze.isDirectory()) {
-                    dirChecker(ze.getName())
-                } else {
-                    val fout = FileOutputStream(_targetLocation + ze.getName())
-                    var c: Int = zin.read()
-                    while (c != -1) {
-                        fout.write(c)
-                        c = zin.read()
-                    }
-                    zin.closeEntry()
-                    fout.close()
-                }
-            }
-            zin.close()
-        } catch (e: Exception) {
-            println(e)
-        }
-    }
-
-    private fun dirChecker(dir: String) {
-        val f = File(dir)
-        if (!f.isDirectory()) {
-            f.mkdirs()
-        }
-    }
-    private const val BUFFER = 80000
 }
 
