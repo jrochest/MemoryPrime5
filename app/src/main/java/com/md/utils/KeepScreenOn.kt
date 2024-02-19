d5e34b408031ed1b31a26e9bd8954e2efbc48596
jacobrochester <jrochest@gmail.com>
2024-02-19 14:13:44 -0800

    KeepScreenOn convert to Kotlin

--- app/src/main/java/com/md/utils/KeepScreenOn.kt
+++ app/src/main/java/com/md/utils/KeepScreenOn.kt
@@ -1,40 +1,35 @@
-package com.md.utils;
+package com.md.utils
 
-import android.os.Handler;
-import android.view.WindowManager;
+import android.os.Handler
+import android.view.WindowManager
+import com.md.SpacedRepeaterActivity
 
-import com.md.SpacedRepeaterActivity;
-
-public class KeepScreenOn {
-    private static KeepScreenOn instance = null;
-
-    private int sequenceNumber = 0;
-
-    private static final int TIMEOUT_MILLIS = 15 * 60 * 1000;
-
-    private KeepScreenOn() {
-    }
-
-    public static KeepScreenOn getInstance() {
-        if (instance == null) {
-            instance = new KeepScreenOn();
-        }
-        return instance;
-    }
-
-    public void keepScreenOn(final SpacedRepeaterActivity activity) {
-        sequenceNumber++;
-        activity.getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
-        final int requestId = sequenceNumber;
-        new Handler().postDelayed(() -> {
-            if (activity.isDestroyed() || activity.isFinishing()) {
-                return;
+class KeepScreenOn private constructor() {
+    private var sequenceNumber = 0
+    fun keepScreenOn(activity: SpacedRepeaterActivity) {
+        sequenceNumber++
+        activity.window.addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON)
+        val requestId = sequenceNumber
+        Handler().postDelayed({
+            if (activity.isDestroyed || activity.isFinishing) {
+                return@postDelayed
             }
-
             if (requestId == sequenceNumber) {
                 // TIMEOUT_MILLIS without a new command so remove the screen on flag.
-                activity.getWindow().clearFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
+                activity.window.clearFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON)
             }
-        }, TIMEOUT_MILLIS);
+        }, TIMEOUT_MILLIS.toLong())
+    }
+
+    companion object {
+        var instance: KeepScreenOn? = null
+            get() {
+                if (field == null) {
+                    field = KeepScreenOn()
+                }
+                return field
+            }
+            private set
+        private const val TIMEOUT_MILLIS = 15 * 60 * 1000
     }
 }
