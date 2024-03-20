80d398342906d7712a93df19de07477cdc8d5862
jacobrochester <jrochest@gmail.com>
2024-03-19 18:49:17 -0700

    Clean up more recording unused code

--- app/src/main/java/com/md/composeModes/RecordAgainButton.kt
+++ app/src/main/java/com/md/composeModes/RecordAgainButton.kt
@@ -1,46 +1,12 @@
 package com.md.composeModes
 
-import android.content.Context
 import androidx.compose.foundation.layout.Column
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import com.md.AudioRecorder
-import com.md.SpacedRepeaterActivity
-import dagger.hilt.android.qualifiers.ActivityContext
-import dagger.hilt.android.scopes.ActivityScoped
-import javax.inject.Inject
 
-
-@ActivityScoped
-class RecordButtonController @Inject constructor(
-    @ActivityContext val context: Context,
-    private val currentNotePartManager: CurrentNotePartManager
-) {
-    val activity: SpacedRepeaterActivity by lazy {
-        context as SpacedRepeaterActivity
-    }
-    private var audioRecorder: AudioRecorder? = null
-
-    fun onTripleTap() {
-        val recorder = audioRecorder
-        if (recorder != null) {
-            recorder.stop()
-            if (recorder.isRecorded) {
-                val filename = recorder.originalFile
-                currentNotePartManager.updateAudioFilename(filename)
-            }
-
-        } else {
-            val recorder = AudioRecorder()
-            this.audioRecorder = recorder
-            recorder.start()
-        }
-
-    }
-}
 
 @Composable
 fun UnlockRecordButton(
